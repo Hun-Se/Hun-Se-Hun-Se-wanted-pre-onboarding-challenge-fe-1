45f159fba63b8461779cfe84aaa02bf37ae53954
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-01-08 05:55:41 +0900

    로그인, 회원가입 이메일 및 패스워드 유효성검증 기능추가 및 유효성 메시지 스타일 추가 + 회원가입 성공 시 로그인 창이동 기능 추가

--- client/src/components/Login/LoginForm.tsx
+++ client/src/components/Login/LoginForm.tsx
@@ -1,5 +1,4 @@
 import React, { useState, Dispatch, SetStateAction } from "react";
-import { render } from "react-dom";
 import { useNavigate } from "react-router-dom";
 import classes from "./LoginForm.module.css";
 
@@ -12,16 +11,44 @@ const LoginForm = (props: loginFromProps) => {
   const [enteredPassword, setEnteredPassword] = useState<string>("");
   const navigate = useNavigate();
 
+  const [emailMessage, setEmailMessage] = useState<string>("");
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+
+  const [emailValidation, setEmailValidation] = useState<boolean>(false);
+  const [passwordValidation, setPasswordValidation] = useState<boolean>(false);
+
   const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setEnteredEmail(event.target.value);
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = event.target.value;
+    setEnteredEmail(emailCurrent);
+
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage("올바른 이메일 형식을 입력해주세요!");
+      setEmailValidation(false);
+    } else {
+      setEmailMessage("올바른 이메일 형식입니다");
+      setEmailValidation(true);
+    }
   };
 
   const passwordChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    event.preventDefault();
-    setEnteredPassword(event.target.value);
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    const passwordCurrent = event.target.value;
+    setEnteredPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage(
+        "숫자 + 영문 + 특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+      setPasswordValidation(false);
+    } else {
+      setPasswordMessage("올바른 패스워드 형식입니다!");
+      setPasswordValidation(true);
+    }
   };
 
   const onLoginHandler = (event: React.MouseEvent) => {
@@ -59,14 +86,44 @@ const LoginForm = (props: loginFromProps) => {
       <form className={classes["form-login"]}>
         <h1>로그인</h1>
         <label htmlFor="loginemail">이메일</label>
+        {enteredEmail.length > 0 && (
+          <span
+            className={`${classes["email-message"]} ${
+              emailValidation ? classes.success : classes.error
+            }`}
+          >
+            {emailMessage}
+          </span>
+        )}
         <input id="loginemail" type="email" onChange={emailChangeHandler} />
+
         <label htmlFor="loginpassword">패스워드</label>
+        {enteredPassword.length > 0 && (
+          <span
+            className={`${classes["password-message"]} ${
+              passwordValidation ? classes.success : classes.error
+            }`}
+          >
+            {passwordMessage}
+          </span>
+        )}
         <input
           id="loginpassword"
-          type="text"
+          type="password"
           onChange={passwordChangeHandler}
         />
-        <button onClick={onLoginHandler}>로그인</button>
+
+        <button
+          onClick={onLoginHandler}
+          disabled={!(emailValidation && passwordValidation)}
+          className={
+            !(emailValidation && passwordValidation)
+              ? classes["button-disabled"]
+              : ""
+          }
+        >
+          로그인
+        </button>
         <button onClick={moveSignUpHandler}>회원가입</button>
       </form>
     </>
--- client/src/components/SignUp/SignUpForm.tsx
+++ client/src/components/SignUp/SignUpForm.tsx
@@ -9,16 +9,44 @@ const SignUpForm = (props: signUpProps) => {
   const [enteredEmail, setEnteredEmail] = useState<string>("");
   const [enteredPassword, setEnteredPassword] = useState<string>("");
 
+  const [emailMessage, setEmailMessage] = useState<string>("");
+  const [passwordMessage, setPasswordMessage] = useState<string>("");
+
+  const [emailValidation, setEmailValidation] = useState<boolean>(false);
+  const [passwordValidation, setPasswordValidation] = useState<boolean>(false);
+
   const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setEnteredEmail(event.target.value);
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = event.target.value;
+    setEnteredEmail(emailCurrent);
+
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage("올바른 이메일 형식을 입력해주세요!");
+      setEmailValidation(false);
+    } else {
+      setEmailMessage("올바른 이메일 형식입니다");
+      setEmailValidation(true);
+    }
   };
 
   const passwordChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    event.preventDefault();
-    setEnteredPassword(event.target.value);
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    const passwordCurrent = event.target.value;
+    setEnteredPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage(
+        "숫자 + 영문 + 특수문자 조합으로 8자리 이상 입력해주세요!"
+      );
+      setPasswordValidation(false);
+    } else {
+      setPasswordMessage("올바른 패스워드 형식입니다!");
+      setPasswordValidation(true);
+    }
   };
 
   const signUpHandler = async (event: React.MouseEvent<HTMLElement>) => {
@@ -41,6 +69,7 @@ const SignUpForm = (props: signUpProps) => {
         .then((res) => {
           if (res.message) alert(res.message);
           if (res.details) alert(res.details);
+          backLoginHandler(event);
         });
     } catch (e) {
       console.error(e);
@@ -57,15 +86,43 @@ const SignUpForm = (props: signUpProps) => {
       <form className={classes["form-signup"]}>
         <h1>회원가입</h1>
         <label htmlFor="signupemail">이메일</label>
+        {enteredEmail.length > 0 && (
+          <span
+            className={`${classes["email-message"]} ${
+              emailValidation ? classes.success : classes.error
+            }`}
+          >
+            {emailMessage}
+          </span>
+        )}
         <input id="signupemail" type="email" onChange={emailChangeHandler} />
         <label htmlFor="signupPassword">패스워드</label>
+        {enteredPassword.length > 0 && (
+          <span
+            className={`${classes["password-message"]} ${
+              passwordValidation ? classes.success : classes.error
+            }`}
+          >
+            {passwordMessage}
+          </span>
+        )}
         <input
           id="signupPassword"
           type="password"
           onChange={passwordChangeHandler}
         />
+        <button
+          onClick={signUpHandler}
+          disabled={!(emailValidation && passwordValidation)}
+          className={
+            !(emailValidation && passwordValidation)
+              ? classes["button-disabled"]
+              : ""
+          }
+        >
+          회원가입
+        </button>
         <button onClick={backLoginHandler}>로그인페이지 돌아가기</button>
-        <button onClick={signUpHandler}>회원가입</button>
       </form>
     </>
   );
