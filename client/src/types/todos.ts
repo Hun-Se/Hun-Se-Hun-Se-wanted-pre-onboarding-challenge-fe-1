0629683edda1ee68fe6dfd3b5f2dd94d3e3d39b9
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-01-31 16:01:45 +0900

    투두 타입모델 추가

--- client/src/types/todos.ts
+++ client/src/types/todos.ts
@@ -11,3 +11,13 @@ export type TodoFormType = Pick<TodoType, "title" | "content">;
 export type CreateTodoType = TodoFormType;
 
 export type UpdateTodoType = Pick<TodoType, "id" | "title" | "content">;
+
+export type DeleteTodoType = Pick<TodoType, "id" | "title" | "content">;
+
+export interface ShownModalHandlerType {
+  onShown: () => void;
+}
+
+export interface HideModalHandlerType {
+  onHide: () => void;
+}
