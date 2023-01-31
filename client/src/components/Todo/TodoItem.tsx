662ac95a13648fad61568cd888509d5254da6eb8
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-01-31 15:57:00 +0900

    TodoItem 뷰, 로직 분리 및 상세내용 모달창 기능 추가

--- client/src/components/Todo/TodoItem.tsx
+++ client/src/components/Todo/TodoItem.tsx
@@ -1,52 +1,47 @@
-import React, { useState } from "react";
-import TodoModel from "../../models/todo";
-// import { DtailTodoObject } from "../../pages/TodoPage";
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import DtailTodo from "./DtailTodo/DtailTodo";
+import API_PATH from "../../constant/api_path_constant";
+import { useDeleteTodo } from "../../hook/queries/todo_query";
+import useModalHandler from "../../hook/todos/useModalHandler";
 import classes from "./TodoItem.module.css";
 
-interface propsType {
-  id: string;
-  header: HeadersInit;
+interface TodoItemProps {
   title: string;
+  id: string;
   content: string;
-  onRemoveTodo: () => void;
-  // onOpen: () => void;
-  setDtailTodo: React.Dispatch<React.SetStateAction<Object>>;
 }
-const TodoItem = (props: propsType) => {
+
+const TodoItem = (props: TodoItemProps) => {
+  const navigate = useNavigate();
+  const { todoModalShown, showDtailTodoHandler, hideDtailTodoHandler } =
+    useModalHandler();
+  const { mutate: DeleteMutate } = useDeleteTodo();
+
   const removeHanler = (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      fetch(`http://localhost:8080/todos/${props.id}`, {
-        method: "DELETE",
-        headers: props.header,
-        body: JSON.stringify({ data: null }),
-      }).then((res) => res.json());
-    } catch (e) {
-      console.error(e);
-    }
-    props.onRemoveTodo();
+    DeleteMutate(props.id);
   };
 
   const getDtailTodohandler = (event: React.MouseEvent) => {
     event.preventDefault();
-
-    fetch(`http://localhost:8080/todos/${props.id}`, {
-      method: "Get",
-      headers: props.header,
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        props.setDtailTodo(res.data);
-      });
-    // props.onOpen();
+    navigate(API_PATH.TODO_BY_ID(props.id));
+    showDtailTodoHandler();
   };
 
   return (
     <>
+      {todoModalShown && (
+        <DtailTodo
+          id={props.id}
+          title={props.title}
+          content={props.content}
+          onHiden={hideDtailTodoHandler}
+        />
+      )}
       <li className={classes["container-list"]}>
-        <div>{props.title}</div>
-        <div>{props.content}</div>
+        <div className={classes["list-title"]}>{props.title}</div>
+        <div className={classes["list-content"]}>{props.content}</div>
         <div className={classes["container-todo-button"]}>
           <button
             className={classes["button-dtail"]}
